abaaabab5bd1259d5ec509c28fad9352da40fea4
pizarrosch <bayenrshc89@gmail.com>
2023-10-18 23:16:44 +0200

    Create footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "./components/Header/Header";
 import s from './App.module.scss';
 import MainPage from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className={s.root}>
-      <Header />
-        <MainPage />
-    </div>
-  );
+    return (
+        <div className={s.root}>
+            <Header/>
+            <MainPage/>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import footerLogo from '../../assets/footer-logo.svg';
+import s from './Footer.module.scss';
+
+export default function Footer() {
+    return (
+        <footer className={s.footer}>
+            <img src={footerLogo} alt='company logo'/>
+            <div className={s.contactsDataContainer}>
+                <div className={s.contactsData}>
+                    <span>г. Москва, Цветной б-р, 40</span>
+                    <span>+7 495 771 21 11</span>
+                    <span>info@skan.ru</span>
+                </div>
+                <span className={s.copyright}>Copyright. 2022</span>
+            </div>
+        </footer>
+    )
+}
